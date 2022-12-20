4ec7e0a3604f63ffa67fb2a8c16b447ae41d767b
dlwnsghks123 <117975195+dlwnsghks123@users.noreply.github.com>
2022-12-20 21:51:47 +0900

    #
    #

--- src/main/java/com/bitstudy/app/domain/ArticleComment.java
+++ src/main/java/com/bitstudy/app/domain/ArticleComment.java
@@ -1,5 +1,4 @@
 package com.bitstudy.app.domain;
-
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -11,69 +10,44 @@ import org.springframework.data.annotation.LastModifiedDate;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@Getter
-@ToString
-@Entity /*
-          테이블과의 매핑 한다는뜻
-          JPA 가 관리한다
-          PK키 알아볼수있게 필드들중 하나에 @Id어노테이션 달아줘야한다.
-*/
-
 @Table(indexes = {
         @Index(columnList = "content"),
-        @Index(columnList = "createdAt"),
-        @Index(columnList = "createdBy"),
+        @Index(columnList = "createAt"),
+        @Index(columnList = "createBy")
 })
+@Entity
+@Getter
+@ToString
 public class ArticleComment {
-
-
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
+    private long id; // 게시글 고유 아이디
 
     @Setter
-    @ManyToOne(optional = false) private  Article articleId;
-    //private Article article;
-    /*
-    연관관계 매핑
-    연관관계 없이 만들면 private Long articleId; 이런식으로 (관계형 데이터베이스 스타일) 하면된다
-    그런데 우리는 Article 과 ArticleCommnet가 관계를 맺고 있는걸 객체지향적으로 표현하려고 이렇게 쓸거다.
-    그러기 위해서 필요한건 단방향 이라는 뜻의 @ManyToOne 에너테이션을 써주고
-    이건 필수값이다 라는 뜻으로 (optional = false)
-    '댓글은 여러개 : 게시글 1개' 이기때문에 단방향 방삭을 썻다.
+    @ManyToOne(optional = false) // 필수 값이라는 뜻
+    private Article article;
 
-    */
-
- //   private String title; //제목 //   private String hashtag; //해시태그
 
     @Setter
     @Column(nullable = false, length = 500)
-    private String content; //본문
-
+    private String content; // 본문
 
     //메타데이터
     @CreatedDate
     @Column(nullable = false)
-    private LocalDateTime createdAt; //생성일시
-
+    private LocalDateTime createAt; // 생성일자
 
     @CreatedBy
-    @Column(nullable = false, length = 100)
-    private String createdBy; //생성자
-
+    @Column(nullable = false,length = 100)
+    private String createBy; // 생성자
 
     @LastModifiedDate
     @Column(nullable = false)
-    private LocalDateTime modifiedAt; //수정일시
-
+    private LocalDateTime modifiedAt; // 수정일자
 
     @LastModifiedBy
-    @Column(nullable = false, length = 100)
-    private String modifiedBt; //수정일시
-
+    @Column(nullable = false,length = 100)
+    private String modifiedBy; // 수정자
 
 
 }
-
